6d080684e2000ebd5b377fff212f720407c08c91
vanick <provanick2023@gmail.com>
2026-04-10 01:15:25 -0400

    fix: comment out redis connection setup in redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,8 +1,10 @@
+/*
 import { Redis } from "ioredis";
 
 export const redisConnection = new Redis({
   host: "127.0.0.1",
   port: 6379,
   maxRetriesPerRequest: null,
-}); */
+}); 
+*/
 export const redisConnection = null;
